50ab624b467245010296b033c593cdefdd4768bd
Pasan Laksitha <pasanlaksithakulathunge@gmail.com>
2021-06-25 17:57:37 +0530

    Add files via upload

---
+++ get-nonFollower-list.py
@@ -0,0 +1,23 @@
+from instapy import InstaPy
+
+# login credentials
+insta_username = ''
+insta_password = ''
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username, password=insta_password)
+session.login()
+print("logged")
+
+get_nonfollowers = session.pick_nonfollowers(username="pasan.creation", live_match=True, store_locally=False)
+# now, `pasan.creation_nonfollowers` variable which is a list- holds the `Nonfollowers` data of "pasan.creation" at requested time
+
+print(get_nonfollowers)
+non = get_nonfollowers
+
+print("\n".join(non))
+f = open("get_nonfollowers.txt", 'w')
+f.write("\n".join(non))
+
+print("saved as get_nonfollowers.txt")
